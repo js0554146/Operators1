3e9311c63020a77229bd9ac6a04e9f147b24ec95
js0554146 <js0554146@gmail.com>
2025-04-15 19:50:10 -0400

    Avrg of trees

---
+++ Avrg.py
@@ -0,0 +1,15 @@
+# Finding the sum of all the trees
+tree1=98
+tree2=92
+tree3=48
+tree4=41
+tree5=11
+
+sum = tree1+tree2+tree3+tree4+tree5
+
+print("The sum of all the trees is:", sum)
+# Finding the average for all the trees
+
+average = sum/5
+
+print("The average of all the trees is:", average)
